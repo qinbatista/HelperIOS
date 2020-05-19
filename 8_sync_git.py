37e843bc19d34e57ce4a6c10635c2be5996e2121
qin yu peng <qin.batista@outlook.com>
2020-05-19 14:37:28 +0800

    更新同步文件夹

--- 8_sync_git.py
+++ 8_sync_git.py
@@ -3,6 +3,11 @@ import shutil
 def PythonLocation():
 	return os.path.dirname(os.path.realpath(__file__))
 
+def	__list_folder(_path):
+	List = []
+	for i in os.listdir(_path):
+		List.append(i)
+	return List
 def main():
 	os.chdir(PythonLocation())
 	with open(f"./CodeAddress.txt",encoding="utf8") as file_object:
@@ -11,6 +16,15 @@ def main():
 			if i.find("git clone ssh://")!=-1:
 				print("downloading codes")
 				os.system(i)
+	folder_names = __list_folder("./")
+	for folder_name in folder_names:
+		if os.path.isdir(PythonLocation()+"/"+folder_name):
+			print("syncing "+folder_name)
+			os.chdir("./"+folder_name)
+			os.system("git pull")
+			os.chdir("./../")
+
+
 
 if __name__ == '__main__':
 	main()
